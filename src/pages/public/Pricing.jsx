8b1dc5ca82d951c9a9845f7559c04d382d58e8ee
hasibul-99 <hasibul_uap@outlook.com>
2025-10-03 17:01:04 +0600

    update

--- src/pages/public/Pricing.jsx
+++ src/pages/public/Pricing.jsx
@@ -1,6 +1,6 @@
 import CTA from "../../components/pages/home/CTA";
 import CustomerInteractions from "../../components/pages/home/CustomerInteractions";
-import ComparePlans from "../../components/pages/pricing/comparePlans";
+import ComparePlans from "../../components/pages/pricing/ComparePlans";
 import Hero from "../../components/pages/pricing/Hero";
 
 export default function Pricing() {
